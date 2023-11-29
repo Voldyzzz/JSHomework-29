ff8e3388b43fd794a7e6d9ac2d5b551914224e0f
Voldyzzz <volodymyr.liuliev2000@gmail.com>
2023-11-29 15:26:49 +0200

    /

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 const bodyRef = document.querySelector("body"),
-  square = document.createElement("div");
+  square = document.createElement("div"),
+  textInSquare = document.createElement("p");
+
 square.classList.add("square");
+textInSquare.classList.add("text", "hidden");
+textInSquare.innerText = "БЕМС";
 
 bodyRef.appendChild(square);
-
-let numLeft = 50;
-const maxLeftValue = 100;
+square.appendChild(textInSquare);
 
 window.addEventListener("keydown", (e) => {
-  let ts = window
-    .getComputedStyle(square, null)
-    .getPropertyValue("Left")
-    .slice(0, 3);
-  square.style.Left = ts;
+  textInSquare.classList.remove("fade-out");
+  square.classList.remove("jump-animation", "ctrl-animation");
 
   if (e.key == "ArrowRight") {
-    square.style.left = `${numLeft}%`;
-    if (numLeft !== 0) {
-      numLeft -= 1;
+    square.style.left = `${square.offsetLeft}px`;
+    if (square.offsetLeft + 250 < window.innerWidth) {
+      square.style.left = `${square.offsetLeft + 10}px`;
+    } else {
+      square.style.left = `${square.offsetLeft - 20}px`;
+      textInSquare.classList.add("fade-out");
     }
-
-    console.log("square.clientLeft", square.scrollLeft);
   } else if (e.key == "ArrowLeft") {
-    square.style.left = `${numLeft}%`;
-    if (numLeft !== 100) {
-      numLeft += 1;
+    square.style.left = `${square.offsetLeft}px`;
+    if (square.offsetLeft >= 250) {
+      square.style.left = `${square.offsetLeft - 10}px`;
+    } else {
+      square.style.left = `${square.offsetLeft + 20}px`;
+      textInSquare.classList.add("fade-out");
     }
+  } else if (e.code == "Space") {
+    square.classList.add("jump-animation");
+  } else if (e.key == "Control") {
+    square.classList.add("ctrl-animation");
   }
 });
